313602af5d35c773108482ac4ae25c2c6f9af669
Komiljon7 <kruziyev0@gmail.com>
2026-08-31 19:22:30 +0500

    izoh yoziladi

---
+++ 1-kun/1-kun.py
@@ -0,0 +1,21 @@
+# python this interpreted language
+# in the python all things are objects and objects has 3 things  1) ID 2) TYPE 3) VALUE
+# if you write a = 5 and then a = b 5 isn'thange  create new object
+a = 5
+b = 12
+print(a)
+a = b
+print(b)
+print(a)
+
+# mutability has two types 1 immutable (o'zgarmas) > int, float,  string, tuple and bool 2) mutable (o'zgaruvchan) dict, list and set
+
+list1 = [1, 2, 'komiljon']
+list1.append('roziyev') # this add end of list and change list don't create new object
+
+# == = its equality operator and this compares the value two objects
+# is = identity operator and this compares their physical memory identity
+
+# is and =
+number = [1,2,3,4]
+name = ["komiljon", "roziyev"]
