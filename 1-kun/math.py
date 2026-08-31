046a74b4fccdb38a8423cdf85b2d6d36dca03fc7
Komiljon7 <kruziyev0@gmail.com>
2026-08-31 19:42:08 +0500

    basic math

---
+++ 1-kun/math.py
@@ -0,0 +1,25 @@
+# math in python 
+# outpit = tenglik additon qo'shish subtraction = ayirish multiplication ko'paytirish, True Division	always returns a float= faqat qoldiq chiqoradi	
+# Floor Division = rounds down to nearest integer faqat asosiy raqamni chiqoradi , modulo = returns the division remainder qoldiqni aynan chiqoradi
+# exponentoation =  darajaga ko'tarish
+
+#  + addition, - subtraction, * multiplication , / = True Division, // (Floor Division) , % Modulo, ** Exponentiation
+
+
+qoshish = 5 + 10
+print(qoshish)
+
+ayirish = 10 - 5
+print(ayirish)
+
+kopaytir = 10 * 9 
+print(kopaytir)
+
+qoldiq = 10 / 2
+print(qoldiq)
+
+butun = 9 // 2
+print(butun)
+
+hisob = 10 + 2 * 5 ** 2
+print(hisob)
